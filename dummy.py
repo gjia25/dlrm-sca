16bb5d131b694a6327beccd886fc41d850f4b372
Grace Jia <gracejia25@gmail.com>
2024-03-14 15:05:42 -0400

    dummy accesses

---
+++ dummy.py
@@ -0,0 +1,22 @@
+import time
+import os
+import signal
+import argparse
+
+NUM_LOOKUPS = 26
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Dummy accesses')
+    parser.add_argument("--parent-pid", type=int)
+    args = parser.parse_args()
+
+    arr1 = list(range(NUM_LOOKUPS))
+
+    for i in range(NUM_LOOKUPS):
+        os.kill(parser.parent_pid, signal.SIGUSR1)
+        time.sleep(1)
+
+        n = arr1[i]
+
+        os.kill(parser.parent_pid, signal.SIGUSR1)
+        time.sleep(1)
