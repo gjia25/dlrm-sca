fd3bebd217702775e3cd4a9c012898513b00da8c
Grace Jia <gracejia25@gmail.com>
2024-03-21 12:14:07 -0400

    plot both pfn, va

--- walkplots/plot_pfns.py
+++ walkplots/plot_pfns.py
@@ -1,5 +1,5 @@
 import os
-from math import ceil
+from math import ceil, floor
 import csv
 import sys
 import matplotlib.pyplot as plt
@@ -7,71 +7,76 @@ import matplotlib.ticker as ticker
 
 filename = sys.argv[1]
 filepath_to_time_to_pfns = {}
+filepath_to_time_to_vas = {}
 
 prefixes = ["/usr/lib/", "/usr/bin/", "/home/gjia/yolo/venv/lib/python3.10/site-packages/"]
+interest = ["[heap]", "[stack]"]
+
 
 # Open the file and read its lines
 with open(filename, "r") as file:
     reader = csv.reader(file)
     # next(reader)  # Skip header
     for row in reader:
-        if len(row) != 4:
-            # print(f'weird row: {repr(row)}')
+        if len(row) != 5:
             continue
         time = int(row[1])
-        hex_value = int(row[2], 16)
-        filepath = row[3]
+        if time > 26: # TODO: delete this block for multi batch
+            break
+        hex_value_virt = int(row[2], 16) # 2 for VA
+        hex_value = int(row[3], 16) # 3 for PFN
+        filepath = row[4]
         
-        for prefix in prefixes: # merging filepaths with same/similar directories
-            if filepath.startswith(prefix):
-                suffix = filepath[len(prefix):]
-                filepath = suffix.split('/')[0].lower()
-                if filepath.endswith(".libs"):
-                    filepath = filepath[:-5]
-                elif filepath.startswith("pil"):
-                    filepath = "pillow"
-                if filepath.startswith("torch"):
-                    filepath = "torch"
-                break
+        if filepath not in interest:
+            continue
         
         if filepath not in filepath_to_time_to_pfns:
             filepath_to_time_to_pfns[filepath] = {}
         if time not in filepath_to_time_to_pfns[filepath]:
-            filepath_to_time_to_pfns[filepath][time] = []
-        filepath_to_time_to_pfns[filepath][time].append(hex_value)
+            filepath_to_time_to_pfns[filepath][time] = set()
+        filepath_to_time_to_pfns[filepath][time].add(hex_value)
 
-# Record all PFNs
-y_values = []
+        if filepath not in filepath_to_time_to_vas:
+            filepath_to_time_to_vas[filepath] = {}
+        if time not in filepath_to_time_to_vas[filepath]:
+            filepath_to_time_to_vas[filepath][time] = set()
+        filepath_to_time_to_vas[filepath][time].add(hex_value_virt)
 
 # Create scatter plot
-fig = plt.figure(figsize=(14,8))
-axes = fig.add_subplot(111)
+def scatterplot(ylabel, filepath_to_time_to_values):
+    fig = plt.figure(figsize=(15,9))
+    axes = fig.add_subplot(111)
 
-NUM_COLORS = len(filepath_to_time_to_pfns)
-cm = plt.get_cmap('gist_ncar')
-axes.set_prop_cycle(color=[cm(i/NUM_COLORS) for i in range(NUM_COLORS)])
-# axes.set_prop_cycle(color=plt.get_cmap('tab20b').colors)
-                         
-for filepath, time_to_pfns in sorted(filepath_to_time_to_pfns.items()):
-    x = []
-    y = []
-    for time, hex_list in time_to_pfns.items():
-        x.extend([time] * len(hex_list))
-        y.extend(hex_list)
-        y_values.extend(hex_list)
-    axes.scatter(x, y, s=2, marker='s', label=filepath)
+    # ylims
+    max_y = 0
+    min_y = sys.maxsize
+                             
+    for filepath, time_to_values in sorted(filepath_to_time_to_values.items()):
+        x = []
+        y = []
+        for time, hex_list in time_to_values.items():
+            x.extend([time] * len(hex_list))
+            y.extend(hex_list)
+            max_y = max(max_y, max(hex_list))
+            min_y = min(min_y, min(hex_list))
+            print(f"accesses to {filepath} in lookup {time}: {len(hex_list)}")
+        axes.scatter(x, y, s=2, marker='s', label=filepath)
 
-axes.set_ylim([0, ceil(max(y_values)/10)*10])
-axes.get_yaxis().set_major_locator(ticker.MultipleLocator(16 ** 4))
-axes.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: '0x%08x' % int(x)))
+    print(f"min_y: {min_y}, max_y: {max_y}")
+    axes.set_ylim([floor(min_y/10)*10, ceil(max_y/10)*10])
+    # axes.get_yaxis().set_major_locator(ticker.MultipleLocator(16 ** 4))
+    axes.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: '0x%08x' % int(x)))
 
-axes.set_xlabel("Lookup #")
-axes.set_ylabel("PFN")
-axes.set_title("Page accesses by each embedding lookup")
+    axes.set_xlabel("Lookup #")
+    axes.set_ylabel(ylabel)
+    axes.set_title("Page accesses by each embedding lookup")
 
-axes.legend(loc='lower center', bbox_to_anchor=(0.5, -0.2), ncol=6, markerscale=4)
+    axes.legend(loc='lower center', bbox_to_anchor=(0.5, -0.2), ncol=6, markerscale=4)
 
-axes.grid(True)
-fig.tight_layout()
-plt.savefig(f"{filename}.pdf")
-plt.show()
+    axes.grid(True)
+    fig.tight_layout()
+    plt.savefig(f"{filename}_{ylabel}.png")
+    plt.show()
+
+scatterplot("PFN", filepath_to_time_to_pfns)
+scatterplot("VA", filepath_to_time_to_vas)
