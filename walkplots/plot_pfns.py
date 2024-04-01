ef1db7f5ad897eb6b419f713f4fe392e10f1f084
Grace Jia <gracejia25@gmail.com>
2024-04-01 13:29:30 -0400

    per inference working set

--- walkplots/plot_pfns.py
+++ walkplots/plot_pfns.py
@@ -21,6 +21,9 @@ def remove_repeated_pages(filepath_to_time_to_values):
         for time, values in time_to_values.items():
             time_to_values[time] = values - repeated_values
 
+def get_processed_filename(filename, pfn_or_va, run):
+    return f"{filename}_{pfn_or_va}_{run}"
+
 # Create scatter plot
 def scatterplot(ylabel, filepath_to_time_to_values, run):
     fig = plt.figure(figsize=(15,9))
@@ -47,24 +50,22 @@ def scatterplot(ylabel, filepath_to_time_to_values, run):
     axes.set_ylim([floor(min_y/10)*10, ceil(max_y/10)*10])
     # axes.get_yaxis().set_major_locator(ticker.MultipleLocator(16 ** 4))
     axes.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: '0x%08x' % int(x)))
-
-    axes.set_xlabel("Lookup #")
     axes.set_ylabel(ylabel)
+
+    axes.set_xlim([0, NUM_RUNS + 1])
+    axes.set_xticks(list(range(1, NUM_RUNS + 1)))
+    axes.set_xlabel("Lookup #")
+    
     axes.set_title("Distinct page accesses per embedding lookup")
 
     axes.legend(loc='lower center', bbox_to_anchor=(0.5, -0.2), ncol=6, markerscale=4)
 
     axes.grid(True)
     fig.tight_layout()
-    plt.savefig(f"{filename}_{ylabel}_{run}.png")
+    plt.savefig(f"{get_processed_filename(filename, ylabel, run)}.png")
     # plt.show()
 
-def task(args):
-    run, filepath_to_time_to_pfns, filepath_to_time_to_vas = args
-    remove_repeated_pages(filepath_to_time_to_pfns)
-    remove_repeated_pages(filepath_to_time_to_vas)
-
-    # Check if there are no distinct page accesses
+def check_no_distinct_pages(filepath_to_time_to_pfns, filepath_to_time_to_vas):
     no_distinct_pfns = True
     no_distinct_vas = True
     for filepath in interest:
@@ -73,22 +74,41 @@ def task(args):
         for time in range(1, NUM_LOOKUPS + 1):
             pfn_timevals = filepath_to_time_to_pfns[filepath].get(time, []) # filepath_to_time_to_pfns[filepath][time]
             va_timevals = filepath_to_time_to_vas[filepath].get(time, []) 
-            print(f"run {run} lookup {time}: {len(pfn_timevals)} PFNs, {len(va_timevals)} VAs, {len(pfn_timevals) == len(va_timevals)}")
+            print(f"run {time}, {filepath}: {len(pfn_timevals)} PFNs, {len(va_timevals)} VAs, {len(pfn_timevals) == len(va_timevals)}")
+            # print(f"run {run}, lookup {time}, {filepath}: {len(pfn_timevals)} PFNs, {len(va_timevals)} VAs, {len(pfn_timevals) == len(va_timevals)}")
             no_distinct_pfns = no_distinct_pfns and len(pfn_timevals) == 0
             no_distinct_vas = no_distinct_vas and len(va_timevals) == 0
+    return no_distinct_pfns and no_distinct_vas
+
+def task(args):
+    run, filepath_to_time_to_pfns, filepath_to_time_to_vas = args
+    remove_repeated_pages(filepath_to_time_to_pfns)
+    remove_repeated_pages(filepath_to_time_to_vas)
         
-    if no_distinct_pfns and no_distinct_vas:
+    if check_no_distinct_pages(filepath_to_time_to_pfns, filepath_to_time_to_vas):
         return run, False
     
-    scatterplot("PFN", filepath_to_time_to_pfns, run)
-    scatterplot("VA", filepath_to_time_to_vas, run)
+    scatterplot("PFN", filepath_to_run_to_pfns, "overall")
+    scatterplot("VA", filepath_to_run_to_vas, "overall")    
+    # scatterplot("PFN", filepath_to_time_to_pfns, run)
+    # scatterplot("VA", filepath_to_time_to_vas, run)
     return run, True
+
+def get_filepath_to_run_to_values(lst_filepath_to_time_to_values):
+    filepath_to_run_to_values = {
+        filepath: {run: set() for run in range(NUM_RUNS)}
+        for filepath in interest
+    }
+    for run, filepath_to_time_to_values in enumerate(lst_filepath_to_time_to_values):
+        for filepath, time_to_values in filepath_to_time_to_values.items():
+            for time, values in time_to_values.items():
+                filepath_to_run_to_values[filepath][run] |= values
+    print(filepath_to_run_to_values)
+    return filepath_to_run_to_values
 
 if __name__ == '__main__':
     lst_filepath_to_time_to_pfns = [{} for _ in range(NUM_RUNS)]
     lst_filepath_to_time_to_vas = [{} for _ in range(NUM_RUNS)]
-
-    prefixes = ["/usr/lib/", "/usr/bin/", "/home/gjia/yolo/venv/lib/python3.10/site-packages/"]
 
     # Open the file and read its lines
     with open(filename, "r") as file:
@@ -99,7 +119,8 @@ if __name__ == '__main__':
                 continue
             run = (int(row[1]) - 1) // NUM_LOOKUPS
             time = int(row[1]) % NUM_LOOKUPS or NUM_LOOKUPS
-
+            if run >= NUM_RUNS:
+                break
             hex_value_virt = int(row[2], 16) # 2 for VA
             hex_value = int(row[3], 16) # 3 for PFN
             filepath = row[4]
@@ -121,8 +142,30 @@ if __name__ == '__main__':
                 filepath_to_time_to_vas[filepath][time] = set()
             filepath_to_time_to_vas[filepath][time].add(hex_value_virt)
 
-    print("Parsed output file")
+    print(f"Parsed output file: expected {NUM_RUNS} runs, got {len(lst_filepath_to_time_to_pfns)}")
 
-    inputs = zip(range(NUM_RUNS), lst_filepath_to_time_to_pfns, lst_filepath_to_time_to_vas)
-    with multiprocessing.Pool(processes=NUM_RUNS) as pool: 
-        print(pool.map(task, inputs))
+    filepath_to_run_to_pfns = get_filepath_to_run_to_values(lst_filepath_to_time_to_pfns)
+    del lst_filepath_to_time_to_pfns
+    filepath_to_run_to_vas = get_filepath_to_run_to_values(lst_filepath_to_time_to_vas)
+    del lst_filepath_to_time_to_vas
+    print("De-duplicating pages across lookups")
+    remove_repeated_pages(filepath_to_run_to_pfns)
+    remove_repeated_pages(filepath_to_run_to_vas)
+    if check_no_distinct_pages(filepath_to_run_to_pfns, filepath_to_run_to_vas):
+        print("No distinct pages accessed across inference requests :(")
+        sys.exit(0)
+
+    scatterplot("PFN", filepath_to_run_to_pfns, "overall")
+    scatterplot("VA", filepath_to_run_to_vas, "overall")
+
+    # inputs = zip(range(NUM_RUNS), lst_filepath_to_time_to_pfns, lst_filepath_to_time_to_vas)
+    # with multiprocessing.Pool(processes=NUM_RUNS) as pool: 
+    #     print(pool.map(task, inputs))
+    # for input in inputs:
+    #     run, filepath_to_time_to_pfns, filepath_to_time_to_vas = input
+    #     if run > 0:
+    #         del prev_pfns
+    #         del prev_vas
+    #     task(input)
+    #     prev_pfns = filepath_to_time_to_pfns
+    #     prev_vas = filepath_to_time_to_vas
