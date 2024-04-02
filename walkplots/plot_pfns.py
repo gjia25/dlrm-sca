2a0bd8c0d40a28c80d8599c366abf4cdd8118a81
Grace Jia <gracejia25@gmail.com>
2024-04-02 11:06:19 -0400

    vas/pfns only

--- walkplots/plot_pfns.py
+++ walkplots/plot_pfns.py
@@ -14,13 +14,6 @@ NUM_LOOKUPS = eval(sys.argv[2]) # 26 for kaggle
 NUM_RUNS = eval(sys.argv[3]) # 3 for kaggle
 interest = ["[heap]", "[stack]"]
 
-# Remove pages accessed across all lookups
-def remove_repeated_pages(filepath_to_time_to_values):
-    for filepath, time_to_values in filepath_to_time_to_values.items():
-        repeated_values = set.intersection(*time_to_values.values())
-        for time, values in time_to_values.items():
-            time_to_values[time] = values - repeated_values
-
 def get_processed_filename(filename, pfn_or_va, run):
     return f"{filename}_{pfn_or_va}_{run}"
 
@@ -28,11 +21,12 @@ def get_processed_filename(filename, pfn_or_va, run):
 def scatterplot(ylabel, filepath_to_time_to_values, run):
     fig = plt.figure(figsize=(15,9))
     axes = fig.add_subplot(111)
-
+    axes.grid(True)
+    
     # ylims
     max_y = 0
     min_y = sys.maxsize
-                             
+                                
     for filepath, time_to_values in sorted(filepath_to_time_to_values.items()):
         x = []
         y = []
@@ -52,20 +46,19 @@ def scatterplot(ylabel, filepath_to_time_to_values, run):
     axes.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: '0x%08x' % int(x)))
     axes.set_ylabel(ylabel)
 
-    axes.set_xlim([0, NUM_RUNS + 1])
-    axes.set_xticks(list(range(1, NUM_RUNS + 1)))
-    axes.set_xlabel("Lookup #")
+    axes.set_xlim([-0.99, NUM_RUNS - 0.01])
+    axes.set_xticks(list(range(0, NUM_RUNS)))
+    axes.set_xlabel("Inference #")
     
     axes.set_title("Distinct page accesses per embedding lookup")
 
     axes.legend(loc='lower center', bbox_to_anchor=(0.5, -0.2), ncol=6, markerscale=4)
 
-    axes.grid(True)
     fig.tight_layout()
     plt.savefig(f"{get_processed_filename(filename, ylabel, run)}.png")
     # plt.show()
 
-def check_no_distinct_pages(filepath_to_time_to_pfns, filepath_to_time_to_vas):
+def check_no_distinct_pages_both(filepath_to_time_to_pfns, filepath_to_time_to_vas):
     no_distinct_pfns = True
     no_distinct_vas = True
     for filepath in interest:
@@ -79,6 +72,33 @@ def check_no_distinct_pages(filepath_to_time_to_pfns, filepath_to_time_to_vas):
             no_distinct_pfns = no_distinct_pfns and len(pfn_timevals) == 0
             no_distinct_vas = no_distinct_vas and len(va_timevals) == 0
     return no_distinct_pfns and no_distinct_vas
+
+# Remove pages accessed across all lookups
+def remove_repeated_pages(filepath_to_time_to_values):
+    for filepath, time_to_values in filepath_to_time_to_values.items():
+        repeated_values = set.intersection(*time_to_values.values())
+        for time, values in time_to_values.items():
+            time_to_values[time] = values - repeated_values
+    print(repeated_values)
+
+def check_no_distinct_pages(filepath_to_run_to_pfns):
+    no_distinct_pfns = True
+    for filepath in interest:
+        if filepath not in filepath_to_run_to_pfns:
+            continue
+        for run in range(NUM_RUNS):
+            pfn_timevals = filepath_to_run_to_pfns[filepath].get(run, []) # filepath_to_time_to_pfns[filepath][time]
+            print(f"run {run}, {filepath}: {len(pfn_timevals)} PFNs")
+            no_distinct_pfns = no_distinct_pfns and len(pfn_timevals) == 0
+    return no_distinct_pfns
+
+def save_distinct_pages(filepath_to_time_to_values, pfn_or_va):
+    with open(f"{get_processed_filename(filename, pfn_or_va, "distinct")}", "w") as file:
+        writer = csv.writer(file)
+        for filepath, time_to_values in filepath_to_time_to_values.items():
+            for time, values in time_to_values.items():
+                for value in values:
+                    writer.writerow([filepath, time, value])
 
 def task(args):
     run, filepath_to_time_to_pfns, filepath_to_time_to_vas = args
@@ -103,7 +123,6 @@ def get_filepath_to_run_to_values(lst_filepath_to_time_to_values):
         for filepath, time_to_values in filepath_to_time_to_values.items():
             for time, values in time_to_values.items():
                 filepath_to_run_to_values[filepath][run] |= values
-    print(filepath_to_run_to_values)
     return filepath_to_run_to_values
 
 if __name__ == '__main__':
@@ -128,12 +147,12 @@ if __name__ == '__main__':
             if filepath not in interest:
                 continue
             
-            filepath_to_time_to_pfns = lst_filepath_to_time_to_pfns[run]
-            if filepath not in filepath_to_time_to_pfns:
-                filepath_to_time_to_pfns[filepath] = {}
-            if time not in filepath_to_time_to_pfns[filepath]:
-                filepath_to_time_to_pfns[filepath][time] = set()
-            filepath_to_time_to_pfns[filepath][time].add(hex_value)
+            # filepath_to_time_to_pfns = lst_filepath_to_time_to_pfns[run]
+            # if filepath not in filepath_to_time_to_pfns:
+            #     filepath_to_time_to_pfns[filepath] = {}
+            # if time not in filepath_to_time_to_pfns[filepath]:
+            #     filepath_to_time_to_pfns[filepath][time] = set()
+            # filepath_to_time_to_pfns[filepath][time].add(hex_value)
 
             filepath_to_time_to_vas = lst_filepath_to_time_to_vas[run]
             if filepath not in filepath_to_time_to_vas:
@@ -144,18 +163,18 @@ if __name__ == '__main__':
 
     print(f"Parsed output file: expected {NUM_RUNS} runs, got {len(lst_filepath_to_time_to_pfns)}")
 
-    filepath_to_run_to_pfns = get_filepath_to_run_to_values(lst_filepath_to_time_to_pfns)
-    del lst_filepath_to_time_to_pfns
+    # filepath_to_run_to_pfns = get_filepath_to_run_to_values(lst_filepath_to_time_to_pfns)
+    # del lst_filepath_to_time_to_pfns
     filepath_to_run_to_vas = get_filepath_to_run_to_values(lst_filepath_to_time_to_vas)
     del lst_filepath_to_time_to_vas
     print("De-duplicating pages across lookups")
-    remove_repeated_pages(filepath_to_run_to_pfns)
+    # remove_repeated_pages(filepath_to_run_to_pfns)
     remove_repeated_pages(filepath_to_run_to_vas)
-    if check_no_distinct_pages(filepath_to_run_to_pfns, filepath_to_run_to_vas):
+    if check_no_distinct_pages(filepath_to_run_to_vas):
         print("No distinct pages accessed across inference requests :(")
         sys.exit(0)
 
-    scatterplot("PFN", filepath_to_run_to_pfns, "overall")
+    # scatterplot("PFN", filepath_to_run_to_pfns, "overall")
     scatterplot("VA", filepath_to_run_to_vas, "overall")
 
     # inputs = zip(range(NUM_RUNS), lst_filepath_to_time_to_pfns, lst_filepath_to_time_to_vas)
